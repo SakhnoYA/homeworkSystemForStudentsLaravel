ccae84754db15113fe23dc8b2f0f44542c586a7b
Iaroslav Sahno <james.film@mail.ru>
2023-08-07 21:02:40 +0300

    Added js files

---
+++ resources/js/cacheForms.js
@@ -0,0 +1,27 @@
+// window.addEventListener('DOMContentLoaded', () => {
+//     const forms = document.querySelectorAll('form.login__form');
+//
+//     forms.forEach((form) => {
+//         form.addEventListener('submit', () => {
+//             collectAllFormData(forms);
+//         });
+//     });
+// });
+//
+// function collectAllFormData(forms)
+// {
+//     const formDatas = new FormData();
+//     forms.forEach((form, index) => {
+//         const formData = new FormData(form);
+//         for (const [name, value] of formData.entries()) {
+//             formDatas.append(`form${index}[${name}]`, value);
+//         }
+//     })
+//
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('POST', '/php/common/saveFormsData.php', true);
+//
+//     xhr.send(formDatas);
+//
+// }
+//
---
+++ resources/js/checkHomework.js
@@ -0,0 +1,53 @@
+// window.addEventListener('DOMContentLoaded', () => {
+//     const button = document.querySelector('button[name="toSendHomework"]');
+//
+//     button.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         collectAllFormData();
+//     });
+// });
+//
+// function collectAllFormData()
+// {
+//     const forms = document.querySelectorAll('form.login__form');
+//     const formDatas = new FormData();
+//
+//     forms.forEach((form, index) => {
+//         const formData = new FormData(form);
+//         const mergedValues = {};
+//
+//         for (const [name, value] of formData.entries()) {
+//             if (name.endsWith('[]')) {
+//                 const fieldName = `task${index}[${name.slice(0, -2)}][]`;
+//                 mergedValues[fieldName] = (mergedValues[fieldName] || []).concat(value);
+//             } else {
+//                 formDatas.append(`task${index}[${name}]`, value);
+//             }
+//         }
+//
+//         for (const [name, values] of Object.entries(mergedValues)) {
+//             const mergedValue = values.join(' ');
+//             formDatas.append(name, mergedValue);
+//         }
+//     });
+//
+//     const requestOptions = {
+//         method: 'POST',
+//         body: formDatas,
+//     };
+//
+//     fetch('/php/common/checkHomework.php' + window.location.search, requestOptions)
+//         .then((response) => {
+//             if (response.ok) {
+//                 return response.json();
+//             } else {
+//                 throw new Error('Json error.');
+//             }
+//         })
+//         .then((data) => {
+//             window.location.href = '/php/common/resultHomework.php?attempt_id=' + data.attempt_id;
+//         })
+//         .catch((error) => {
+//             console.error(error);
+//         });
+// }
