63ab159476d578e35809ea66d83d3f752048a50c
Iaroslav Sahno <james.film@mail.ru>
2023-08-07 21:02:57 +0300

    Created migrations

--- database/migrations/2023_08_07_160000_create_users_table.php
+++ database/migrations/2023_08_07_160000_create_users_table.php
@@ -12,14 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->integer('id')->primary();
+            $table->string('first_name', 30);
+            $table->string('last_name', 30);
+            $table->string('middle_name', 30)->nullable();
+            $table->string('password',255);
+            $table->unsignedSmallInteger('type');
+            $table->ipAddress('ip')->nullable();
+            $table->boolean('is_confirmed')->default(false);
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('type')->references('id')->on('user_types');
         });
+
     }
 
     /**
